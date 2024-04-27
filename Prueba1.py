7960bb49518e33c5e7251562729cd2f7d9bc1a61
josue-suarez <josuemsuarez@gmail.com>
2024-04-27 21:27:01 +0200

    vesion 0.3

--- Prueba1.py
+++ Prueba1.py
@@ -1,13 +1,28 @@
 # Solicitamos nombre y edad al usuario
 nombre = input("introduzca su nombre: ")
-edad = int(input("introduzca s edad: "))
+edad = int(input("introduzca su edad: "))
 
-# Comnprobamos si es mayor de edad, y además, si está casado
-casado = False
+# Comnprobamos si está casado
+while True:
+    casado = input("Indique si está casado [C] o soltero [S]: ")
+    if casado.upper() == "C":
+        casado = True
+        break
+    elif casado.upper() == "S":
+        casado = False
+        break
+    else:
+        print("""Opción no válida.
+            Indique C si está casado,
+            o S si está soltero. """)
+
+# Mostramos informacón en pantalla
 print(f"Bienvenido {nombre}")
-if edad >= 18 and casado:
+if edad >= 18 and not casado:
     print("Eres mayor de edad y estás soltero")
-elif edad >= 18 and not casado:
+elif edad >= 18 and casado:
     print("Eres mayor de edad y estás casado")
+elif edad < 18 and not casado:
+    print("No eres mayor de edad y estás soltero")
 else:
-    print("No eres mayor de edad")
+    print("No eres mayor de edad y estás casado")
