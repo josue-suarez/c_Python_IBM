c06d6cd523425676841f8bacfbe1b990d1274201
josue-suarez <josuemsuarez@gmail.com>
2024-04-27 20:58:37 +0200

    version 0.2

--- Prueba1.py
+++ Prueba1.py
@@ -1,10 +1,13 @@
-nombre = "Andrés"
-edad = 28
+# Solicitamos nombre y edad al usuario
+nombre = input("introduzca su nombre: ")
+edad = int(input("introduzca s edad: "))
+
+# Comnprobamos si es mayor de edad, y además, si está casado
 casado = False
 print(f"Bienvenido {nombre}")
-if edad >= 18 and casado == False:
+if edad >= 18 and casado:
     print("Eres mayor de edad y estás soltero")
-elif edad >= 18 and casado == True:
+elif edad >= 18 and not casado:
     print("Eres mayor de edad y estás casado")
 else:
     print("No eres mayor de edad")
