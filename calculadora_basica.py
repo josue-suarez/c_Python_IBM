be3db5a9a191dc851cd10247f12b74f27c2f1236
josue-suarez <josuemsuarez@gmail.com>
2024-05-01 12:39:45 +0200

    v0.2

---
+++ calculadora_basica.py
@@ -0,0 +1,12 @@
+S_TITULO = "CALCULADORA OPERACIONES BÁSICAS"
+print(S_TITULO)
+for i in range(16):
+    print("_", end=" ")
+
+print("""\nMenú
+      1. Suma
+      2. Resta
+      3. Multiplicación
+      4. División
+      5. Módulo / Resto
+      6. Potencia""")
